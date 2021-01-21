758e415315f801c34b8cb2c2c207cd91717935d6
Adrian Talavera <renato210596@gmail.com>
2021-01-21 16:25:48 -0500

    v.1

---
+++ Desktop/apirestcrud/database/migrations/2021_01_19_030747_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->String('name');
+            $table->String('description');
+            $table->String('genre');
+            $table->integer('year');
+            $table->String('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * 
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
